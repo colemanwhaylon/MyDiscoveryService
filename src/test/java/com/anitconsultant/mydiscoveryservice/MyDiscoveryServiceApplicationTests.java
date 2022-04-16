3d5023810acdc8e1eb646adbd2d5b51b5e48a4e4
Whaylon Coleman <colemanwhaylon@yahoo.com>
2022-04-16 15:18:10 -0700

    Initial commit.

---
+++ src/test/java/com/anitconsultant/mydiscoveryservice/MyDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anitconsultant.mydiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
